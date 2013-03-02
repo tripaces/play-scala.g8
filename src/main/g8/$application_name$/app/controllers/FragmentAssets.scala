f6af06a4230041a94658118e84ee5ea43f3a6059
Greg Bougeard <gbougeard@gmail.com>
2013-03-02 18:50:31 +0100

    Remove println

--- src/main/g8/$application_name$/app/controllers/FragmentAssets.scala
+++ src/main/g8/$application_name$/app/controllers/FragmentAssets.scala
@@ -7,7 +7,6 @@ object FragmentAssets extends Controller {
   val FRAGMENTS_PATH_PREFIX = List("public", "fragments")
 
   def at(file: String): Action[AnyContent] = {
-    println("/" + FRAGMENTS_PATH_PREFIX.mkString("/")+file)
     Assets.at("/" + FRAGMENTS_PATH_PREFIX.mkString("/"), file)
   }
 }
